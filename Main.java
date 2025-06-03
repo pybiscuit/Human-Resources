f989f4eab321a1085e76fa8c1304a32d76e6e944
pybiscuit <chemistryrob@gmail.com>
2025-06-03 13:10:51 -0600

    added comments

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+// Name: Robert Vanderburg
+// Email: rvanderburg@unm.edu
+// Assignment: Main.java (Human Resources)
+// Purpose: Human Resource Parts 1 and 2. provide mechanisms for inheritance, 
+// inerfaces, polymorphism, and file io.
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,23 +16,23 @@ answer the following questions:
 
 Q1: Car and Engine are related
 by which, Inheritance or Composition?
-Composition. Engine is an Attribute of Car.
+Composition. Car "has-a" engine.
 
 Q2: Color and Red are related
 by which, Inheritance or Composition?
-Inheritance. Red is a sub-class of Color.
+Inheritance. Red "is-a" Color.
 
 Q3: Shirt and Clothing are related
 by which, Inheritance or Composition?
-Inheritance. Shirt is a sub-class of Clothing.
+Inheritance. Shirt "is-a" Clothing.
 
 Q4: Furniture and Desk are related
 by which, Inheritance or Composition?
-Inheritance. Desk is a sub-class of Furniture.
+Inheritance. Desk "is-a" Furniture.
 
 Q5: CellPhone and Battery are related
 by which, Inheritance or Composition?
-Composition. Battery is an attribute of CellPhone.
+Composition. CellPhone "has-a" Battery.
 
 */
 
@@ -34,13 +40,18 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
+
 		File inputFile = new File(args[0]);
 		char skipFirstRow = 'Y';
 		Person newPerson = new Person("Marcus", 72, 111);
 		PersonSet listOfPeople = new PersonSet();
+
 		try {
+
 			Scanner scnr = new Scanner(inputFile);
+
 			while (scnr.hasNextLine()) {
+
 				if (skipFirstRow == 'Y') {
                     skipFirstRow = 'N';
 					scnr.nextLine();
@@ -53,13 +64,15 @@ public class Main
 				
 				Person tmpPerson = new Person(name, height, weight);
 				listOfPeople.add(tmpPerson);
-				//if (scnr.hasNextLine()) scnr.nextLine();
 			}
+
 			scnr.close();
+
 		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
-	
+			
 		System.out.print(listOfPeople);		
 	}
 }
